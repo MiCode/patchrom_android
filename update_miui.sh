02d019001cba35ab89340fdf53a58b1e46a07e7d
wangwenjun <wangwenjun@xiaomi.com>
2012-03-28 10:29:44 +0800

    modify script for merge and verify miui
    Change-Id: I3154e673ee92c37d6803204e10875f07ed2d1f5d

--- update_miui.sh
+++ update_miui.sh
@@ -4,39 +4,20 @@ RMLINE=$PORT_ROOT/tools/rmline.sh
 APKTOOL=$PORT_ROOT/tools/apktool
 
 rm -rf temp
-mkdir temp
-mkdir temp/android
-mkdir temp/miui
+mkdir -p temp/android
+mkdir -o temp/miui
 
-function save_framework_for_diff() {
-	echo "save current framework for diff"
-	rm -rf last-framework/*
-	cp -r framework.jar.out last-framework/
-	cp -r services.jar.out last-framework/
-	cp -r android.policy.jar.out last-framework/
-}
-
-
-function copy_and_remove_lines() {
-	echo "remove .line from temp/android/$1"
-	cp -r $1 temp/android
-	$RMLINE temp/android/$1
-}
-
-save_framework_for_diff
-
-copy_and_remove_lines framework.jar.out
-copy_and_remove_lines services.jar.out
-copy_and_remove_lines android.policy.jar.out
-
-function disassemble_and_remove_lines() {
-	echo "remove .line from temp/miui/$1.out"
-	$APKTOOL d $1
-	$RMLINE $1.out
-}
+cp -r *.jar.out temp/android
 
 cd temp/miui
-cp $ANDROID_OUT/system/framework/*.jar .
-disassemble_and_remove_lines framework.jar
-disassemble_and_remove_lines services.jar
-disassemble_and_remove_lines android.policy.jar
+cp $ANDROID_OUT/system/framework/framework.jar .
+cp $ANDROID_OUT/system/framework/services.jar .
+cp $ANDROID_OUT/system/framework/android.policy.jar .
+
+$APKTOOL d framework.jar
+$APKTOOL d services.jar
+$APKTOOL d android.policy.jar
+
+cd -
+
+$RMLINE temp
--- verify.sh
+++ verify.sh
@@ -1,6 +1,6 @@
 adb remount
 make clean
-make .build/framework.jar-phone
-make .build/android.policy.jar-phone
-make .build/services.jar-phone
+make out/framework.jar-phone
+make out/android.policy.jar-phone
+make out/services.jar-phone
 adb shell stop;adb shell start
