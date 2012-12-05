61d00a08eb85e99a9e30e38f79002bd9eba8d09b
yumingyang <yumingyang@xiaomi.com>
2012-12-05 14:34:45 +0800

    update update_miui.sh and verify.sh
    Change-Id: I808abed912dae95caf3e5fb660615445fe1c6b04

--- update_miui.sh
+++ update_miui.sh
@@ -3,23 +3,80 @@
 RMLINE=$PORT_ROOT/tools/rmline.sh
 APKTOOL=$PORT_ROOT/tools/apktool
 
-rm -rf temp
-mkdir -p temp/android
+function disassemble_and_remove_lines() {
+	echo "remove .line from temp/miui/$1.out"
+	$APKTOOL d $1 > /dev/null 2>/dev/null
+	$RMLINE $1.out
+}
+
 mkdir -p temp/miui
-
-cp -r *.jar.out temp/android
-
 cd temp/miui
-cp $ANDROID_OUT/system/framework/framework.jar .
-cp $ANDROID_OUT/system/framework/services.jar .
-cp $ANDROID_OUT/system/framework/android.policy.jar .
 
-$APKTOOL d framework.jar
-$APKTOOL d services.jar
-$APKTOOL d android.policy.jar
+if [ -z "$ANDROID_OUT" ]
+then
+	echo "Pls setup environment firstly"
+	exit 1
+fi
 
-cd -
+if [ $# -eq 0 ]
+then
+	echo "update miui-all"
+	rm -rf android.jar.out framework.jar.out services.jar.out
+	cp  $ANDROID_OUT/system/framework/android.policy.jar .
+	cp  $ANDROID_OUT/system/framework/framework.jar .
+	cp  $ANDROID_OUT/system/framework/services.jar .
+	disassemble_and_remove_lines framework.jar
+	disassemble_and_remove_lines services.jar
+	disassemble_and_remove_lines android.policy.jar
+	if [ -f $ANDROID_OUT/system/framework/framework2.jar ]
+	then
+		echo "merge framewrok2.jar.out to framewrok.jar.out"
+		rm -rf framework2.jar.out
+		cp  $ANDROID_OUT/system/framework/framework2.jar .
+		disassemble_and_remove_lines framework2.jar
+		cp -r framework2.jar.out/* framework.jar.out/
+		rm -rf framework2.jar.out
+	fi
+else
+	until [ $# -eq 0 ]
+	do
+		case $1 in
+			android.policy*)
+				echo "update miui-android"
+				rm -rf android.jar.out
+				cp  $ANDROID_OUT/system/framework/android.policy.jar .
+				disassemble_and_remove_lines android.policy.jar
+				shift
+				;;
+			framework*)
+				echo "update miui-framework"
+				rm -rf framework.jar.out
+				cp $ANDROID_OUT/system/framework/framework.jar .
+				disassemble_and_remove_lines framework.jar
+				if [ -f $ANDROID_OUT/system/framework/framework2.jar ]
+				then
+					echo "merge framewrok2.jar.out to framewrok.jar.out"
+					rm -rf framework2.jar.out
+					cp  $ANDROID_OUT/system/framework/framework2.jar .
+					disassemble_and_remove_lines framework2.jar
+					cp -r framework2.jar.out/* framework.jar.out/
+					rm -rf framework2.jar.out
+				fi
+				shift
+				;;
+			services*)
+				echo "update miui-services"
+				rm -rf services.jar.out
+				cp  $ANDROID_OUT/system/framework/services.jar .
+				disassemble_and_remove_lines services.jar
+				shift
+				;;
+			*)
+				echo "Error: $1"
+				shift
+				;;
+		esac
+	done
+fi
 
-$RMLINE temp
-cd temp
-find . -name "*.diff" -exec rm {} \;
+
--- verify.sh
+++ verify.sh
@@ -1,6 +1,12 @@
+adb root
+sleep 1
 adb remount
-make clean
+
+rm -rf out/framework.jar out/services.jar out/android.policy.jar out/framework2.jar
 make out/framework.jar-phone
+make out/framework2.jar-phone
 make out/android.policy.jar-phone
 make out/services.jar-phone
-adb shell stop;adb shell start
+adb reboot
+
+
